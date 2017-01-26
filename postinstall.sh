033062872db5e5112439abf086ee2be7df323f8c
Stuart Moore <stuart.moore@gmail.com>
2017-01-26 12:00:11 +0000

    Create a backup of install logs

--- postinstall.sh
+++ postinstall.sh
@@ -5,3 +5,7 @@ esxcli vsan network ipv4 set -i vmk0 -d 224.0.0.220 -u 224.0.0.221
 
 # Rename local data store
 vim-cmd hostsvc/datastore/rename datastore1 "$(hostname -s)-local"
+
+# Create a backup of install related log files
+cp /var/log/hostd.log  "/vmfs/volumes/$(hostname -s)-local-storage-1/firstboot-hostd.log"
+cp /var/log/esxi_install.log "/vmfs/volumes/$(hostname -s)-local-storage-1/firstboot-esxi_install.log"
