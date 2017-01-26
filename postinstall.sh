5b9ef9ff57671830fc6883c67b0d5aa16100db1d
Stuart Moore <stuart.moore@gmail.com>
2017-01-26 16:22:24 +0000

    Lets see if we can add binaries ...

--- postinstall.sh
+++ postinstall.sh
@@ -10,5 +10,5 @@ vim-cmd hostsvc/vmotion/vnic_set vmk0
 esxcli vsan network ipv4 set -i vmk0 -d 224.0.0.220 -u 224.0.0.221
 
 # Create a backup of install related log files
-cp /var/log/hostd.log  "/vmfs/volumes/$(hostname -s)-local-storage-1/firstboot-hostd.log"
-cp /var/log/esxi_install.log "/vmfs/volumes/$(hostname -s)-local-storage-1/firstboot-esxi_install.log"
+cp /var/log/hostd.log  "/vmfs/volumes/$(hostname -s)-local/logs/firstboot-hostd.log"
+cp /var/log/esxi_install.log "/vmfs/volumes/$(hostname -s)-local/logs/firstboot-esxi_install.log"
