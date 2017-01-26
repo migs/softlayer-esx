182547b667ee323933321a2bfd66262992aa6ea5
Stuart Moore <stuart.moore@gmail.com>
2017-01-26 11:53:59 +0000

    Added local datastore rename

--- postinstall.sh
+++ postinstall.sh
@@ -2,3 +2,6 @@
 
 # Softlayer requires changing the vSAN broadcast domain
 esxcli vsan network ipv4 set -i vmk0 -d 224.0.0.220 -u 224.0.0.221
+
+# Rename local data store
+vim-cmd hostsvc/datastore/rename datastore1 "$(hostname -s)-local"
