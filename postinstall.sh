9e925a75c07dfea9cdbad19f96530254ff6fad1b
Stuart Moore <stuart.moore@gmail.com>
2017-01-26 12:54:54 +0000

    Tweaks

--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-# Softlayer requires changing the vSAN broadcast domain
-esxcli vsan network ipv4 set -i vmk0 -d 224.0.0.220 -u 224.0.0.221
-
 # Rename local data store
 vim-cmd hostsvc/datastore/rename datastore1 "$(hostname -s)-local"
+
+# Enable vMotion
+vim-cmd hostsvc/vmotion/vnic_set vmk0
+
+# Softlayer requires changing the vSAN broadcast domain
+esxcli vsan network ipv4 set -i vmk0 -d 224.0.0.220 -u 224.0.0.221
 
 # Create a backup of install related log files
 cp /var/log/hostd.log  "/vmfs/volumes/$(hostname -s)-local-storage-1/firstboot-hostd.log"
